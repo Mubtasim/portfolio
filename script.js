b94e6fac45ce91bbb9b413fba944bfacad9a14a0
Mubtasim Shahriar <mubtasim91@gmail.com>
2020-10-15 23:48:14 +0600

    many change

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let para = document.getElementById("para");
-// para.style.transition = "font-size 0.5s linear 1s";
 let texts = ["hi", "i'm", "mubtasim<br>shahriar", "a regular", "competitive<br>programmer", "max rating<br>expert!", "in codeforces!","4 star", "in codechef"];
 let len = texts.length;
 let idx = 0;
@@ -8,7 +7,6 @@ let interval = setInterval(textSetter,4000);
 
 
 function textSetter() {
-    // para.classList.remove("decFont");
     para.classList.add("incFont");
     para.innerHTML = texts[idx++%len];
     setTimeout(() => {
